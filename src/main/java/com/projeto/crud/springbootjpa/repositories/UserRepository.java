f9a926d657e064a23c40104ba645d4e4e52867ac
TaylanTorres09 <taylantorres@gmail.com>
2022-10-18 16:38:06 -0300

    Create UserRepository

---
+++ src/main/java/com/projeto/crud/springbootjpa/repositories/UserRepository.java
@@ -0,0 +1,11 @@
+package com.projeto.crud.springbootjpa.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.projeto.crud.springbootjpa.models.User;
+
+@Repository
+public interface UserRepository extends JpaRepository<User, Long> {
+    
+}

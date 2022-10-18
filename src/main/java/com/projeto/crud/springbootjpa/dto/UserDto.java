4876dfe9320fdb9db1075f5fcef297360937d0f8
TaylanTorres09 <taylantorres@gmail.com>
2022-10-18 16:34:01 -0300

    Add dto to User

--- src/main/java/com/projeto/crud/springbootjpa/dto/UserDto.java
+++ src/main/java/com/projeto/crud/springbootjpa/dto/UserDto.java
@@ -1,5 +1,23 @@
 package com.projeto.crud.springbootjpa.dto;
 
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
 public class UserDto {
     
+    @NotBlank
+    private String name;
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String phone;
+
+    @NotBlank
+    private String password;
+
 }

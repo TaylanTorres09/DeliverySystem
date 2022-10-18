cb92bd56b4af0ba8946c9642e7c37a4ee542dfe5
TaylanTorres09 <taylantorres@gmail.com>
2022-10-18 16:13:25 -0300

    Create User Model

---
+++ src/main/java/com/projeto/crud/springbootjpa/models/User.java
@@ -0,0 +1,24 @@
+package com.projeto.crud.springbootjpa.models;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Table(name = "users")
+@Data
+public class User {
+    
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    private String name;
+    private String email;
+    private String phone;
+    private String password;
+    
+}

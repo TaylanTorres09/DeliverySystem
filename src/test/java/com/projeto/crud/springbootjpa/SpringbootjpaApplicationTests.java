a2987513371010dcc3b8ab67d176f82fe7de888f
TaylanTorres09 <taylantorres@gmail.com>
2022-10-18 15:48:56 -0300

    Iniciate project Spring Boot

---
+++ src/test/java/com/projeto/crud/springbootjpa/SpringbootjpaApplicationTests.java
@@ -0,0 +1,13 @@
+package com.projeto.crud.springbootjpa;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootjpaApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}

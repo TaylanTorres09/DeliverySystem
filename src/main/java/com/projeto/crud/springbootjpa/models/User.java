3582e89dbd1060ad5d556e1423165354edcf1375
TaylanTorres09 <taylantorres@gmail.com>
2022-10-19 00:14:22 -0300

    Add relationship with order

--- src/main/java/com/projeto/crud/springbootjpa/models/User.java
+++ src/main/java/com/projeto/crud/springbootjpa/models/User.java
@@ -8,6 +8,7 @@ import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.OneToMany;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -26,6 +27,7 @@ public class User implements Serializable {
     private String phone;
     private String password;
 
+    @OneToMany(mappedBy = "client")
     private List<Order> orders = new ArrayList<>();
     
 }

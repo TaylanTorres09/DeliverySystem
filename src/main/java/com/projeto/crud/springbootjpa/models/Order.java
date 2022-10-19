f8266a8f0d2d893b1ce35ae3a164ec08c30a148e
TaylanTorres09 <taylantorres@gmail.com>
2022-10-19 00:38:34 -0300

    Add JsonFormat

--- src/main/java/com/projeto/crud/springbootjpa/models/Order.java
+++ src/main/java/com/projeto/crud/springbootjpa/models/Order.java
@@ -3,6 +3,8 @@ package com.projeto.crud.springbootjpa.models;
 import java.io.Serializable;
 import java.time.Instant;
 
+import com.fasterxml.jackson.annotation.JsonFormat;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -22,6 +24,7 @@ public class Order implements Serializable{
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
 
+    @JsonFormat(shape = JsonFormat.Shape.STRING, pattern = "yyyy-MM-dd'T'HH:mm:ss'Z'", timezone = "GMT")
     private Instant moment;
 
     @ManyToOne

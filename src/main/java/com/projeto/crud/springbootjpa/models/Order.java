60dd71b1fcb0b50643fb59e0ed124690a0040697
TaylanTorres09 <taylantorres@gmail.com>
2022-10-22 00:58:41 -0300

    Add relation to product

--- src/main/java/com/projeto/crud/springbootjpa/models/Order.java
+++ src/main/java/com/projeto/crud/springbootjpa/models/Order.java
@@ -8,6 +8,7 @@ import java.util.Set;
 import com.fasterxml.jackson.annotation.JsonFormat;
 import com.projeto.crud.springbootjpa.models.enums.OrderStatus;
 
+import jakarta.persistence.CascadeType;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
@@ -15,6 +16,7 @@ import jakarta.persistence.Id;
 import jakarta.persistence.JoinColumn;
 import jakarta.persistence.ManyToOne;
 import jakarta.persistence.OneToMany;
+import jakarta.persistence.OneToOne;
 import jakarta.persistence.Table;
 import lombok.Data;
 
@@ -39,5 +41,8 @@ public class Order implements Serializable{
 
     @OneToMany(mappedBy = "id.order")
     private Set<OrderItem> orderItems = new HashSet<>();
+
+    @OneToOne(mappedBy = "order", cascade = CascadeType.ALL)
+    private Payment payment;
     
 }

15056142e0e83d9edb555bf75c7d0babd23eea67
TaylanTorres09 <taylantorres@gmail.com>
2022-10-25 14:13:14 -0300

    Add method findByName

--- src/main/java/com/projeto/crud/springbootjpa/repositories/CategoryRepository.java
+++ src/main/java/com/projeto/crud/springbootjpa/repositories/CategoryRepository.java
@@ -1,6 +1,7 @@
 package com.projeto.crud.springbootjpa.repositories;
 
 import java.util.List;
+import java.util.Optional;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
@@ -11,5 +12,7 @@ import com.projeto.crud.springbootjpa.models.Category;
 public interface CategoryRepository extends JpaRepository<Category, Long> {
 
     List<Category> findCategoriesByProductsId(Long productId);
+
+    Optional<Category> findByName(String name);
     
 }

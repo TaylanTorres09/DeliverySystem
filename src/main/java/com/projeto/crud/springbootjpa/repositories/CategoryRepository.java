fb5a69cd2c8f9bb8647711c5a081a5230c0bd244
TaylanTorres09 <taylantorres@gmail.com>
2022-10-25 00:17:09 -0300

    Add find methods

--- src/main/java/com/projeto/crud/springbootjpa/repositories/CategoryRepository.java
+++ src/main/java/com/projeto/crud/springbootjpa/repositories/CategoryRepository.java
@@ -1,5 +1,7 @@
 package com.projeto.crud.springbootjpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.projeto.crud.springbootjpa.models.Category;
 
 @Repository
 public interface CategoryRepository extends JpaRepository<Category, Long> {
+
+    List<Category> findCategoriesByProductsId(Long productId);
     
 }
--- src/main/java/com/projeto/crud/springbootjpa/repositories/ProductRepository.java
+++ src/main/java/com/projeto/crud/springbootjpa/repositories/ProductRepository.java
@@ -1,5 +1,7 @@
 package com.projeto.crud.springbootjpa.repositories;
 
+import java.util.List;
+
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -7,5 +9,7 @@ import com.projeto.crud.springbootjpa.models.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
+
+    List<Product> findProductsByCategoriesId(Long categoryId);
     
 }

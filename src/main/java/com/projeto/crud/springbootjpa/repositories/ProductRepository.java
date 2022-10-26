5976ab470d7aa37422224c8b7d03d988417c3565
TaylanTorres09 <taylantorres@gmail.com>
2022-10-26 20:07:43 -0300

    Remove query with categoryID

--- src/main/java/com/projeto/crud/springbootjpa/repositories/ProductRepository.java
+++ src/main/java/com/projeto/crud/springbootjpa/repositories/ProductRepository.java
@@ -1,7 +1,5 @@
 package com.projeto.crud.springbootjpa.repositories;
 
-import java.util.List;
-
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
@@ -9,7 +7,5 @@ import com.projeto.crud.springbootjpa.models.Product;
 
 @Repository
 public interface ProductRepository extends JpaRepository<Product, Long> {
-
-    List<Product> findProductsByCategoriesId(Long categoryId);
     
 }

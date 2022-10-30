93b0677c5efa9eb53517de544061a529e116f3e1
TaylanTorres09 <taylantorres@gmail.com>
2022-10-30 16:20:53 -0300

    Create Login DTO

---
+++ src/main/java/com/projeto/crud/springbootjpa/dto/LoginDto.java
@@ -0,0 +1,17 @@
+package com.projeto.crud.springbootjpa.dto;
+
+import jakarta.validation.constraints.Email;
+import jakarta.validation.constraints.NotBlank;
+import lombok.Data;
+
+@Data
+public class LoginDto {
+
+    @NotBlank
+    @Email
+    private String email;
+
+    @NotBlank
+    private String password;
+
+}

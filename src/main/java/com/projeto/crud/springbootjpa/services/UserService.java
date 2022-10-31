ad4b2cd27c141a43789b489aad1e4f6fa94c2306
TaylanTorres09 <taylantorres@gmail.com>
2022-10-31 09:00:54 -0300

    remove unused variables

--- src/main/java/com/projeto/crud/springbootjpa/services/UserService.java
+++ src/main/java/com/projeto/crud/springbootjpa/services/UserService.java
@@ -1,6 +1,5 @@
 package com.projeto.crud.springbootjpa.services;
 
-import java.io.Console;
 import java.util.List;
 import java.util.Optional;
 
@@ -8,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.dao.EmptyResultDataAccessException;
 import org.springframework.http.HttpStatus;
-import org.springframework.http.HttpStatusCode;
 import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
 

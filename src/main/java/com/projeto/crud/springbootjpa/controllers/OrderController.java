2a439736fe8a50179c856955df11c5382e0e9123
TaylanTorres09 <taylantorres@gmail.com>
2022-11-01 15:00:01 -0300

    Configure Cors

--- src/main/java/com/projeto/crud/springbootjpa/controllers/OrderController.java
+++ src/main/java/com/projeto/crud/springbootjpa/controllers/OrderController.java
@@ -5,6 +5,7 @@ import java.util.List;
 import org.springframework.beans.BeanUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -20,6 +21,7 @@ import com.projeto.crud.springbootjpa.services.OrderService;
 
 import jakarta.validation.Valid;
 
+@CrossOrigin("*")
 @RestController
 @RequestMapping("/order")
 public class OrderController {
